877ee0808efbf839b22c9624e49ae41480feb176
SinneR <123@123.mail>
2025-06-14 18:58:25 +0300

    Kotlin-lesson1-task2 Работа с переменными из task1 - вывод в консоль. Добавляем ещё одну переменную и переписываем её значение. Закомментировать строку.

---
+++ src/main/kotlin/lesson_1/lesson1-task2.kt
@@ -0,0 +1,13 @@
+package org.example.lesson_1
+
+fun main() {
+    var sumOfOrders : Int  = 75
+    val thanksMessage: String = "Thanks for your purchase"
+    var numberOfWorkers : Int  = 2000
+    println(sumOfOrders)
+    println(thanksMessage)
+//    println(numberOfWorkers)
+    numberOfWorkers = 1999
+    println(numberOfWorkers)
+
+}
