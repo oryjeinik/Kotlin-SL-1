f5f3e495e3814b55171e3a57e57de348d344164b
SinneR <123@123.mail>
2025-06-15 20:55:38 +0300

    KotlinSprint-lesson1-task5 +Const и поправил форматирвоание

--- src/main/kotlin/lesson_2/lesson2-task1.kt
+++ src/main/kotlin/lesson_2/lesson2-task1.kt
@@ -1,9 +1,10 @@
 package org.example.lesson_2
+const val SUMM_OF_MARK = 4
 
 fun main() {
     val mark1 = 3f
     val mark2 = 4f
     val mark3 = 3f
     val mark4 = 5f
-    println((mark1+mark2+mark3+mark4)/4)
+    println((mark1 + mark2 + mark3 + mark4) / SUMM_OF_MARK)
 }
