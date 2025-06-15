fa1a05160e5ee70b0b5e6f8ab8dfe56c278cf379
SinneR <123@123.mail>
2025-06-15 21:09:27 +0300

    KotlinSprint-lesson2-task3 поправил форматирвоание + const

--- src/main/kotlin/lesson_2/lesson2-task3.kt
+++ src/main/kotlin/lesson_2/lesson2-task3.kt
@@ -1,12 +1,13 @@
 package org.example.lesson_2
+const val MINUTE_IN_HOUR = 60
 
 fun main() {
     val hour = 9
     val minutes = 39
     val minutesOnWay = 457
-    val allTimeInMinutes = hour*60+minutesOnWay+minutes
-    val hourIncoming = allTimeInMinutes/60
-    val minuteAfterHour = allTimeInMinutes%60
+    val allTimeInMinutes = hour * MINUTE_IN_HOUR + minutesOnWay + minutes
+    val hourIncoming = allTimeInMinutes / MINUTE_IN_HOUR
+    val minuteAfterHour = allTimeInMinutes % MINUTE_IN_HOUR
     val formatHour = "%02d".format(hourIncoming)
     val formatMinutes = "%02d".format(minuteAfterHour)
 
