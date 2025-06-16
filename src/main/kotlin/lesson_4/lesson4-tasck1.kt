4a9ca1347b9ddf0092f9593892bd28558363b75f
SinneR <123@123.mail>
2025-06-17 01:07:50 +0300

    KotlinSprint-lesson4-task1 Првоерка доступности столиков в ресторане

--- src/main/kotlin/lesson_4/lesson4-tasck1.kt
+++ src/main/kotlin/lesson_4/lesson4-tasck1.kt
@@ -1,2 +1,10 @@
 package org.example.lesson_4
 
+const val ALL_TABLES = 13
+
+fun main() {
+    val tablesToday  = 13
+    val tablesTomorrow = 9
+    println("[Доступность столиков на на сегодня: ${tablesToday < ALL_TABLES}]\n[Доступность столиков на на завтра: ${tablesTomorrow < ALL_TABLES}]")
+
+}
