1cdf8785822b6b0f3976bdac574ceeb3a1d1661d
SinneR <123@123.mail>
2025-06-17 00:23:42 +0300

    KotlinSprint-lesson2-task5 Формула сложного прцоента(может нагуглил не правильную формулу =)) Исправил. Добавил константу и вычисление процентов

--- src/main/kotlin/lesson_2/lesson2-task5.kt
+++ src/main/kotlin/lesson_2/lesson2-task5.kt
@@ -1,8 +1,10 @@
 package org.example.lesson_2
 
+const val MAX_PROCENT = 100.0
+
 fun main() {
     val initialAmount = 70000.0
-    val annualInterestRate = 0.167 //16.7%
+    val annualInterestRate = 16.7 / MAX_PROCENT //16.7%
     val years = 20
 
     val finalAmout = initialAmount * Math.pow(1 + annualInterestRate, years.toDouble())
