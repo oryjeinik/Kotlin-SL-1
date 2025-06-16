eaf199b02dbe5bd7258e60111d53e91ec3019fcc
SinneR <123@123.mail>
2025-06-17 00:17:08 +0300

    KotlinSprint-lesson2-task4 count interest Исправил. Добавил константу и вычисление процентов

--- src/main/kotlin/lesson_2/lesson2-task4.kt
+++ src/main/kotlin/lesson_2/lesson2-task4.kt
@@ -1,10 +1,12 @@
 package org.example.lesson_2
 
+const val MAX_PROCENT = 100.0
+
 fun main() {
     val crystalOreWithoutBuff = 7
     val ironOreWithoutBuff = 11
 
-    val buffMultiplier = 0.2 // 20% бафф
+    val buffMultiplier = 20.0 / MAX_PROCENT // 20% бафф
 
     val bonusCrystalOre = (crystalOreWithoutBuff * buffMultiplier).toInt()
     val bonusIronOre = (ironOreWithoutBuff * buffMultiplier).toInt()
