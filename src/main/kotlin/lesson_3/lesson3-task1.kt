d16ec19a070429748808d3dba346c8c08764d3de
SinneR <123@123.mail>
2025-06-18 02:03:45 +0300

    KS-3-1 Вывод сообщения "Добрый день" и "Добрый вечер".  объявляю и инициализирую переменную одной строкой(исправил)

--- src/main/kotlin/lesson_3/lesson3-task1.kt
+++ src/main/kotlin/lesson_3/lesson3-task1.kt
@@ -4,9 +4,7 @@ fun main() {
     val userName = "\"Name_of_User\""
     val hiDay = "Добрый день"
     val hiNight = "Доброй ночи"
-    var displayMessage: String
-
-    displayMessage = "$hiDay $userName"
+    var displayMessage = "$hiDay $userName"
     println(displayMessage)
     displayMessage = "$hiNight $userName"
     println(displayMessage)
