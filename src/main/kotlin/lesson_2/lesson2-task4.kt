049f457ae8c5c714a9500a01f7e3d8c42e69d530
SinneR <123@123.mail>
2025-06-18 01:45:52 +0300

    KS-2-4 Переименовал ПР и убрал комментарий

--- src/main/kotlin/lesson_2/lesson2-task4.kt
+++ src/main/kotlin/lesson_2/lesson2-task4.kt
@@ -5,12 +5,10 @@ const val MAX_PROCENT = 100.0
 fun main() {
     val crystalOreWithoutBuff = 7
     val ironOreWithoutBuff = 11
-
     val buffMultiplier = 20.0 / MAX_PROCENT
 
     val bonusCrystalOre = (crystalOreWithoutBuff * buffMultiplier).toInt()
     val bonusIronOre = (ironOreWithoutBuff * buffMultiplier).toInt()
-
     println("Кристаллическая руда: $bonusCrystalOre")
     println("Железная руда: $bonusIronOre")
 }
