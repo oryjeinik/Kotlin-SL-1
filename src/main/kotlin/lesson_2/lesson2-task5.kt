5a905eb53f6a5ee156ea3edf98a6ea4ca848ae55
SinneR <123@123.mail>
2025-06-19 00:03:27 +0300

    KS-2-5 Пправил фОРМУЛУ

--- src/main/kotlin/lesson_2/lesson2-task5.kt
+++ src/main/kotlin/lesson_2/lesson2-task5.kt
@@ -4,10 +4,9 @@ const val MAX_PROCENT = 100.0
 
 fun main() {
     val initialAmount = 70000.0
-    val annualInterestRate = 16.7 / MAX_PROCENT //16.7%
     val years = 20
 
-    val finalAmout = initialAmount * Math.pow(1 + annualInterestRate, years.toDouble())
+    val finalAmout = initialAmount * Math.pow(1 + (16.7 / MAX_PROCENT), years.toDouble())
 
     println("%.3f".format(finalAmout))
 
